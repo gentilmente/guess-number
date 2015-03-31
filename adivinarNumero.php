c498b16c367849342adc700349106511840630cb
pestequiel <pestequiel@gmail.com>
2015-03-31 20:15:57 +0000

    cambios en algoritmo

--- adivinarNumero.php
+++ adivinarNumero.php
@@ -7,40 +7,36 @@ $count = 0;
 if (isset ($_SESSION['counter']))
     $count = $_SESSION['counter'];
 $count++;
-
-$random = rand(1,100);
-
-//if (isset ($_SESSION['random']))
-//    $random = $_SESSION['random'];
-
-if (isset ($_POST['number'])){
+if($count == 1){
+    echo "Bienvenido";
+    $_SESSION['counter'] = $count;
+    $_SESSION["random"] = rand(1, 100);
+} else {
     $respuesta = $_POST['number'];
-} else {
-    $respuesta = -1;
-}
 
-if ($random == $respuesta) {
-    echo " ","Bien !! lo intentaste ", $count, " veces.";
-    session_unset();
-    $count = 0;
-} else {
-    echo " Intento: ", $count, "<br>";
-    if ($count < 4) {
-        if (isset ($_POST['number'])) {
-            if ($random < $respuesta)
-                echo " Tu respuesta es mayor que el número.";
-            else 
-                echo " Tu respuesta es menor que el número";
-        }
-    } else {
-        echo "<h4>Game Over !!</h4> La respuesta correcta es: ", $random;
+    $random = $_SESSION["random"];
+    if ( $random == $respuesta) {
+        echo " ","Bien !! lo intentaste ", $count-1, " veces.";
         session_unset();
         $count = 0;
+    } else {
+        echo " Intento: ", $count-1, "<br>";
+        echo " Tu respuesta: ", $respuesta, "<br>";
+        //echo " Random: ", $random, "<br>";
+        if ($count <= 3) {
+            if ($random < $respuesta)
+                echo " es mayor que mi número.";
+            else 
+                echo " es menor que mi número.";
+                
+            $_SESSION["counter"]=$count;
+        } else {
+            echo "<h4>Game Over !!</h4> La respuesta correcta es: ", $random;
+            session_unset();
+            $count = 0;
+        }
     }
 }
-
-$_SESSION["counter"]=$count;
-//$_SESSION["random"]=$random;
 
 ?>
 
@@ -53,12 +49,16 @@ $_SESSION["counter"]=$count;
 <body>
 
     <form method="post">
-    <label> Adiviná un número entre (1 to 100) </label> <br />
-    
-    <input type="text" name="number" value="<?php if (isset ($_POST['number'])) echo $respuesta, "puto"; ?>" />
-    <br /><br />
-    
-    <input type="submit" value="enviar" name="submit" /> <br />
+    <?php
+    if ($_SESSION['counter'] == 0){
+        echo "";
+    } else {
+        echo "<label> Adiviná un número entre (1 to 100) </label> <br />";
+        echo '<input type="text" name="number" />';
+        echo "<br /><br />";
+    }
+    ?>
+    <input type="submit" value="<?php if($_SESSION['counter'] == 0) echo "Probar otra vez"; else echo "enviar"; ?> " name="submit" /> <br />
     </form>
 </body>
 </html>
